9b81a0065506a75b7d8409a84589bc4dc8eb9fe2
Sergey-ITProgrammer <sergey.rukin@icloud.com>
2022-05-01 21:16:29 +0500

    update VehicleController

--- src/main/java/com/system/carRentalManagementSystem/controller/VehicleController.java
+++ src/main/java/com/system/carRentalManagementSystem/controller/VehicleController.java
@@ -4,6 +4,10 @@ import com.system.carRentalManagementSystem.model.Vehicle;
 import com.system.carRentalManagementSystem.modelAssembler.VehicleModelAssembler;
 import com.system.carRentalManagementSystem.service.VehicleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.IanaLinkRelations;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,42 +26,74 @@ public class VehicleController {
     }
 
     @GetMapping("")
-    public List<Vehicle> getAllVehicles() {
-        return vehicleService.getAllVehicles();
+    public ResponseEntity<?> getAllVehicles() {
+        CollectionModel<EntityModel<Vehicle>> entityModels = modelAssembler.toCollectionModel(vehicleService.getAllVehicles());
+
+        return ResponseEntity.ok(entityModels);
     }
 
     @GetMapping("/{vehicleId}")
-    public Vehicle getVehicleById(@PathVariable("vehicleId") Long vehicleId) {
-        return vehicleService.getVehicleById(vehicleId);
+    public ResponseEntity<?> getVehicleById(@PathVariable("vehicleId") Long vehicleId) {
+        EntityModel<Vehicle> entityModel = modelAssembler.toModel(vehicleService.getVehicleById(vehicleId));
+
+        if(entityModel.getContent() == null) {
+            return ResponseEntity.badRequest().body("Bad request");
+        }
+
+        return ResponseEntity.ok(modelAssembler);
     }
 
     @GetMapping("/available")
-    public List<Vehicle> getAvailableVehicles() {
-        return vehicleService.getAvailableVehicles();
+    public ResponseEntity<?> getAvailableVehicles() {
+        CollectionModel<EntityModel<Vehicle>> entityModels = modelAssembler.toCollectionModel(vehicleService.getAvailableVehicles());
+
+        return ResponseEntity.ok(entityModels);
     }
 
     @GetMapping("/unavailable")
-    public List<Vehicle> getUnavailableVehicles() {
-        return vehicleService.getUnavailableVehicles();
+    public ResponseEntity<?> getUnavailableVehicles() {
+        CollectionModel<EntityModel<Vehicle>> entityModels = modelAssembler.toCollectionModel(vehicleService.getUnavailableVehicles());
+
+        return ResponseEntity.ok(entityModels);
     }
 
     @PostMapping("")
-    public void createVehicle(@RequestBody Vehicle vehicle) {
-        vehicleService.createVehicle(vehicle);
+    public ResponseEntity<?> createVehicle(@RequestBody Vehicle vehicle) {
+        EntityModel<Vehicle> entityModel = modelAssembler.toModel(vehicleService.createVehicle(vehicle));
+
+        if (entityModel.getContent() == null) {
+            return ResponseEntity.badRequest().body("Bad request");
+        }
+
+        return ResponseEntity.created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri()).body(entityModel);
     }
 
     @PutMapping("/{vehicleId}")
-    public void updateVehicle(@RequestBody Vehicle newVehicle, @PathVariable("vehicleId") Long vehicleId) {
-        vehicleService.updateVehicle(newVehicle, vehicleId);
+    public ResponseEntity<?> updateVehicle(@RequestBody Vehicle newVehicle, @PathVariable("vehicleId") Long vehicleId) {
+        EntityModel<Vehicle> entityModel = modelAssembler.toModel(vehicleService.updateVehicle(newVehicle, vehicleId));
+
+        if (entityModel.getContent() == null) {
+            return ResponseEntity.badRequest().body("Bad request");
+        }
+
+        return ResponseEntity.created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri()).body(entityModel);
     }
 
     @PatchMapping("/{vehicleId}")
-    public void updateAvailability(@PathVariable("vehicleId") Long vehicleId, @RequestParam boolean available) {
-        vehicleService.updateAvailabilityById(vehicleId, available);
+    public ResponseEntity<?> updateAvailability(@PathVariable("vehicleId") Long vehicleId, @RequestParam boolean available) {
+        EntityModel<Vehicle> entityModel = modelAssembler.toModel(vehicleService.updateAvailabilityById(vehicleId, available));
+
+        if (entityModel.getContent() == null) {
+            return ResponseEntity.badRequest().body("Bad request");
+        }
+
+        return ResponseEntity.created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri()).body(entityModel);
     }
 
     @DeleteMapping("/{vehicleId}")
-    public void deleteVehicleById(@PathVariable("vehicleId") Long vehicleId) {
+    public ResponseEntity<?> deleteVehicleById(@PathVariable("vehicleId") Long vehicleId) {
         vehicleService.deleteVehicleById(vehicleId);
+
+        return ResponseEntity.noContent().build();
     }
 }
